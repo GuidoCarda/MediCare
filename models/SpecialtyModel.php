c378231a217d916457c8438418eb2b318d0e6bd1
Guido Cardarelli <guidocarda@hotmail.com>
2023-11-02 12:20:43 -0300

    files cleanup

--- models/SpecialtyModel.php
+++ models/SpecialtyModel.php
@@ -1,25 +1,28 @@
-<?php 
+<?php
 
-class SpecialtyModel extends EntityModel{
+class SpecialtyModel extends EntityModel
+{
   protected $table = 'specialty';
   protected $alias = 's';
-  
+
   private $id;
   private $denomination;
 
   // Obtener todas las especialidades
-  public function getAll(){
+  public function getAll()
+  {
     return $this->select('id, denomination');
   }
 
   // Obtener una especialidad por su id
-  public function getById($id){
+  public function getById($id)
+  {
     return $this->select(
       'id, denomination',
       [
-        'where'=> 'id = :id', 
+        'where' => 'id = :id',
         'replaces' => [':id' => $id]
-      ], 
+      ],
       true
     );
   }
